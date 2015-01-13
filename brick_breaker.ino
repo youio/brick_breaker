7e8bee9d6556b3447cc33b8a1042e4a13848e515
youio <quinnferguson48@gmail.com>
2015-01-12 20:54:55 -0500

    initialize brick, pixel transform

--- brick_breaker.ino
+++ brick_breaker.ino
@@ -1,24 +1,34 @@
-# brick_breaker
-good ol' fashion brick breaker 
 
 #include <HT1632.h>
 int ran = 1;
 int rose = 1;
 int x = 8;
 int y = 8;
+const byte BRICK_WIDTH = 16;
+const byte BRICK_HEIGHT = 8;
+const byte BRICK_OFFSET = 16;
+
+const byte MATRIX_WIDTH = 16;
+const byte MATRIX_HEIGHT = 24;
 
 #define DATA 2
 #define WR   3
 #define CS   4
 #define CS2  5
 
+
 HT1632LEDMatrix matrix = HT1632LEDMatrix(DATA, WR, CS);
 
-byte rows[8]= { 1, 2, 3, 4, 5, 6, 7, 8};
-byte cols[16]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
- 11, 12, 13, 16};
+inline void pixelOut(byte x, byte y, byte v) {
+ //matrix.drawPixel(x, y, v); //normal
+  matrix.drawPixel(MATRIX_HEIGHT - x - 1, MATRIX_WIDTH - y - 1, v); //flipped
+}
+
+byte brick[BRICK_HEIGHT][BRICK_WIDTH]; // [rows][columns]
+
  
 void setup() {
+   memset(brick,1,sizeof(brick));
    matrix.begin(HT1632_COMMON_16NMOS);  
 }
 
@@ -27,12 +37,13 @@ void loop() {
   y= y + rose;
   x= x + ran;
   
-   for(int x1 = 0; x <= 8; x1++) {
-   for(int y1 = 0; y1 <= 15; y1++){
-     matrix.drawPixel(rows[x1], cols[y1], 1);
-    if(y == cols[y1] && x == rows[x1] ) {
-    cols[y1] = 26;
+   for(int x1 = 0; x1 <BRICK_HEIGHT; x1++) {
+   for(int y1 = 0; y1 < BRICK_WIDTH; y1++){
+    if(brick[x1][y1]) 
+    {
+    pixelOut(x1 + BRICK_OFFSET, y1, 1);
    }
+ }
  }
   if(y >= 15 || y <= 0) {
    rose = - rose;
@@ -44,7 +55,7 @@ void loop() {
    ran = -ran;
  }
   
-  matrix.drawPixel(x, y, 1);
+  //matrix.drawPixel(x, y, 1);
    matrix.writeScreen();
   delay(100);
   matrix.clearScreen();
